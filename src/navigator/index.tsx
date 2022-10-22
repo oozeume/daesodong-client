0455cdc366c4d28a9c6aa890d982aeea1fee6e10
Jinsu <wlstn8738@naver.com>
2022-10-22 21:22:32 +0900

    [Feat] #3 : 네비게이션 디자인 수정 및 아이콘 라이브러리 추가 - 피그마 기준으로 네비게이션 디자인 수정 - react-native-vector-icons 라이브러리 추가 - 아이콘 사용을 위해 android, ios native에 font 정보 추가

--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -6,6 +6,9 @@ import Test3 from '../pages/test3';
 import Test1 from '../pages/test1';
 import Test2 from '../pages/test2';
 import {Platform} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -16,38 +19,69 @@ const TabNavigator = () => {
       screenOptions={{
         tabBarLabelPosition: 'below-icon',
         tabBarStyle: {
-          backgroundColor: '#28282c',
-          height: 49,
+          backgroundColor: '#FFFFFF',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          height: 98,
         },
         tabBarAllowFontScaling: false,
-        tabBarActiveTintColor: '#40c4ff',
-        tabBarInactiveTintColor: '#fff',
+        tabBarActiveTintColor: '#FF6B00',
+        tabBarInactiveTintColor: '#E1E2E4',
 
         tabBarLabelStyle: {
-          fontWeight: '700',
-          fontSize: 15,
+          fontStyle: 'normal',
+          fontWeight: '500',
+          fontSize: 10,
           marginBottom: Platform.OS === 'android' ? 14 : 0,
-          lineHeight: Platform.OS === 'android' ? 20 : 20,
+          lineHeight: Platform.OS === 'android' ? 14 : 14,
         },
-
-        tabBarIconStyle: {display: 'none'},
       }}>
       <Tab.Screen
         name="Test1"
         component={Test1}
         options={{
-          tabBarLabel: 'Test1',
+          tabBarLabel: '시설 정보',
           headerShown: false,
           unmountOnBlur: true,
+          tabBarIcon: ({color}) => (
+            <FeatherIcon name={'map-pin'} color={color} size={24} />
+          ),
         }}
       />
       <Tab.Screen
         name="Test2"
-        component={Test2}
+        component={Test1}
         options={{
-          tabBarLabel: 'Test2',
+          tabBarLabel: '콘텐츠',
           headerShown: false,
           unmountOnBlur: true,
+          tabBarIcon: ({color}) => (
+            <FontAwesome5Icon name={'book'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Test3"
+        component={Test1}
+        options={{
+          tabBarLabel: '커뮤니티',
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon name={'comments'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Test4"
+        component={Test2}
+        options={{
+          tabBarLabel: '내 계정',
+          headerShown: false,
+          unmountOnBlur: true,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon name={'user'} color={color} size={24} />
+          ),
         }}
       />
     </Tab.Navigator>
