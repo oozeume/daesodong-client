34af80214b065db0c9b54c3aa6f8508cfcbd4dac
Jinsu <wlstn8738@naver.com>
2022-10-23 15:54:27 +0900

    [Style] #3 : View -> HStack 으로 변경 및 불필요한 스타일 코드 제거

--- src/pages/hospital/index.tsx
+++ src/pages/hospital/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Pressable, StyleSheet} from 'react-native';
-import {Text, View} from 'native-base';
+import {Center, HStack, Text, View} from 'native-base';
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {ParamListBase} from '@react-navigation/native';
@@ -29,30 +29,38 @@ const Hospital = ({navigation}: HospitalProps) => {
     <SafeAreaView>
       <View style={styles.container}>
         {/* 병원 상세 타이틀바 */}
-        <View style={styles.topWrapper}>
-          <Pressable style={styles.icon} onPress={() => navigation.goBack()}>
-            <LeftArrowIcon name={'arrow-left'} size={33} />
-          </Pressable>
-          <View style={styles.title}>
+        <HStack space={3} justifyContent="center">
+          <Center h="60" w="30">
+            <Pressable onPress={() => navigation.goBack()}>
+              <LeftArrowIcon name={'arrow-left'} size={33} />
+            </Pressable>
+          </Center>
+          <Center h="60" w="250">
             <Text style={styles.titleText}>병원 이름</Text>
-          </View>
-          <Pressable style={[styles.icon, styles.shareIcon]}>
-            <ShareIcon name={'share-google'} size={33} />
-          </Pressable>
-        </View>
+          </Center>
+          <Center h="60" w="30">
+            <Pressable>
+              <ShareIcon name={'share-google'} size={33} />
+            </Pressable>
+          </Center>
+        </HStack>
         {/* 병원 상세 탭 */}
-        <View style={styles.tabWrapper}>
-          <Pressable
-            style={[styles.tab, selectedTab === 'Info' && styles.tabHilight]}
-            onPress={() => onClickTab('Info')}>
-            <Text style={styles.tabText}>시설 정보</Text>
-          </Pressable>
-          <Pressable
-            style={[styles.tab, selectedTab === 'Review' && styles.tabHilight]}
-            onPress={() => onClickTab('Review')}>
-            <Text style={styles.tabText}>후기(00)</Text>
-          </Pressable>
-        </View>
+        <HStack space={2} justifyContent="center" paddingX={18}>
+          <Center h="52" w="169.5">
+            <Pressable
+              style={[selectedTab === 'Info' && styles.tabHilight]}
+              onPress={() => onClickTab('Info')}>
+              <Text style={styles.tabText}>시설 정보</Text>
+            </Pressable>
+          </Center>
+          <Center h="52" w="169.5">
+            <Pressable
+              style={[selectedTab === 'Review' && styles.tabHilight]}
+              onPress={() => onClickTab('Review')}>
+              <Text style={styles.tabText}>후기(00)</Text>
+            </Pressable>
+          </Center>
+        </HStack>
         {/* 병원 상세 컨텐츠 */}
         {selectedTab === 'Info' ? <HospitalDetail /> : <HospitalReview />}
       </View>
@@ -65,41 +73,11 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#FFFFFF',
   },
-  topWrapper: {
-    width: '100%',
-    height: 68,
-    paddingVertical: 12,
-    paddingHorizontal: 18,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  tabWrapper: {
-    width: '100%',
-    height: 52,
-    paddingVertical: 0,
-    paddingHorizontal: 18,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  title: {
-    flex: 2,
-  },
-  icon: {
-    flex: 1,
-  },
-  shareIcon: {
-    alignItems: 'flex-end',
-  },
   titleText: {
     fontSize: 18,
     fontWeight: '500',
     lineHeight: 26,
     textAlign: 'center',
-  },
-  tab: {
-    height: 52,
-    flex: 1,
-    alignContent: 'center',
   },
   tabText: {
     fontSize: 16,
@@ -108,6 +86,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   tabHilight: {
+    width: '100%',
     borderBottomWidth: 2,
     borderBottomColor: '#000',
   },
