42dcffc3c4670bb164261cd1b7557ac2c652bc69
Jinsu <wlstn8738@naver.com>
2022-10-25 23:07:42 +0900

    [Add] #5 : 병원 시설 정보 탭 이미지 view 생성 - 임시 이미지 리스트 및 타입 생성

--- src/pages/hospital/info/index.tsx
+++ src/pages/hospital/info/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {Center, Divider, Image, ScrollView, VStack} from 'native-base';
+import Swiper from 'react-native-swiper';
 
 import VisitedAnimals from '../../../components/hospital/info/VisitedAnimals';
 import RecordVisitedExperience from '../../../components/hospital/info/RecordVisitedExperience';
+
+// 임시 사진 리스트 및 타입 생성
+type ImageListType = {uri: string};
+
+const TMP_IMAGE_LIST: ImageListType[] = [
+  {uri: 'https://wallpaperaccess.com/full/317501.jpg'},
+  {uri: 'https://www.w3schools.com/css/img_lights.jpg'},
+];
+//
 
 const HospitalReview = () => {
   return (
@@ -10,7 +20,20 @@ const HospitalReview = () => {
       <ScrollView w={'100%'} h={'65%'}>
         {/* 병원 사진 */}
         <Center w="375" h="250" backgroundColor={'#ECECEE'}>
-          <Image width={375} height={250} alt="" />
+          <Swiper
+            dotColor="rgba(26, 30, 39, 0.6)"
+            activeDotColor="#FF6B00"
+            loop={false}>
+            {TMP_IMAGE_LIST.map(image => (
+              <Image
+                key={image.uri}
+                source={{uri: image.uri}}
+                width={375}
+                height={250}
+                alt=""
+              />
+            ))}
+          </Swiper>
         </Center>
         {/* 병원 방문 기록 */}
         <Center>
