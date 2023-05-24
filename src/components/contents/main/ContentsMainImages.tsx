dc0d16e94425ae59768eb484aac12ef0cd6b0630
Woo Jieum <oozeume@gmail.com>
2023-05-24 19:18:24 +0900

    [Refactor] #133 컨텐츠 메인 이미지 스와이퍼 수정
    - 공통 컴포넌트로 변경

--- src/components/contents/main/ContentsMainImages.tsx
+++ src/components/contents/main/ContentsMainImages.tsx
@@ -1,11 +1,8 @@
-import {Box, Circle, Image} from 'native-base';
 import React from 'react';
-import {colors} from '~/theme/theme';
-import Swiper from 'react-native-web-swiper';
-import {APP_WIDTH} from '~/utils/dimension';
 import Content from '~/model/content';
 import {useGetMainContents} from '~/api/contents/queries';
 import {CONTENTS_PER_PAGE} from '~/constants/contents';
+import FullImageSwiper from '~/components/common/swiper/FullImageSwiper';
 
 /**
  *@description 컨텐츠 메인 페이지 상단 (이미지)
@@ -15,49 +12,7 @@ function ContentsMainImages() {
 
   const contents = data?.data.map(c => new Content(c)) ?? [];
 
-  return (
-    <Box>
-      <Swiper
-        controlsProps={{
-          nextTitle: '',
-          prevTitle: '',
-          dotsWrapperStyle: {
-            marginBottom: 20,
-          },
-          DotComponent: ({isActive}) => (
-            <Circle
-              width={'6px'}
-              margin={'3px'}
-              height={'6px'}
-              backgroundColor={
-                isActive ? colors.fussOrange[0] : colors.scrim[60]
-              }
-            />
-          ),
-        }}
-        containerStyle={{
-          width: APP_WIDTH,
-          height: APP_WIDTH,
-        }}>
-        {contents.map(i => (
-          <Image
-            w={'100%'}
-            h={'100%'}
-            src={i.representiveImage}
-            alt={i.id}
-            fallbackElement={
-              <Box
-                width={'100%'}
-                height={'100%'}
-                bgColor={colors.grayScale[40]}
-                mb="24px"
-              />
-            }
-          />
-        ))}
-      </Swiper>
-    </Box>
-  );
+  return <FullImageSwiper images={contents.map(i => i.representiveImage)} />;
 }
 
 export default ContentsMainImages;
