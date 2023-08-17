0f9d6163e0a1f94ca7f8e9a351a5a41655ca9599
yunyami <yunyami0605@gmail.com>
2023-08-17 12:36:04 +0900

    feat: 안드로이드 뒤로가기 훅 추가

---
+++ src/hooks/useBackHandler.ts
@@ -0,0 +1,50 @@
+import React, {useEffect, useRef} from 'react';
+import useToastShow from './useToast';
+import {BackHandler} from 'react-native';
+
+/**
+ *@description 안드로이드 뒤로가기 두번 누를 경우 로직 훅
+ */
+function useBackHandler() {
+  const doubleClickTimeoutCheckRef = useRef<number | null>(null);
+  const doubleClickRef = useRef(false);
+  const {toastShow} = useToastShow();
+
+  // 뒤로가기 두 번시, 앱 종료 이벤트
+  const onDoubleClickBackButton = () => {
+    // 2000(2초) 안에 back 버튼을 한번 더 클릭 할 경우 앱 종료
+
+    if (!doubleClickRef.current) {
+      toastShow('한번 더 누르시면 종료됩니다.');
+      doubleClickRef.current = true;
+
+      doubleClickTimeoutCheckRef.current = setTimeout(
+        () => {
+          doubleClickRef.current = false;
+        },
+        2000, // 2초
+      );
+    } else {
+      if (doubleClickTimeoutCheckRef.current)
+        clearTimeout(doubleClickTimeoutCheckRef.current);
+
+      BackHandler.exitApp(); // 앱 종료
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onDoubleClickBackButton);
+
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        onDoubleClickBackButton,
+      );
+    };
+  }, []);
+
+  return;
+}
+
+export default useBackHandler;
